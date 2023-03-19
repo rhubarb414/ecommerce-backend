656c1f4276b61ee43360438cf1355151583e8928
rhubarb414 <ctophr.dubois@gmail.com>
2023-03-19 07:57:04 -0700

    Adds get route for showing one product by ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require("../../models");
 
-// The `/api/products` endpoint
-
-// get all products
+// Get all products
 router.get("/", async (req, res) => {
   try {
     const allProducts = await Product.findAll({
@@ -16,13 +14,25 @@ router.get("/", async (req, res) => {
   }
 });
 
-// get one product
-router.get("/:id", (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// Get one product
+router.get("/:id", async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "Product ID not found." });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
-// create new product
+// Create new product
 router.post("/", (req, res) => {
   /* req.body should look like this...
     {
@@ -54,7 +64,7 @@ router.post("/", (req, res) => {
     });
 });
 
-// update product
+// Update product
 router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -96,8 +106,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
-});
+// Delete one product by its `id` value
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
